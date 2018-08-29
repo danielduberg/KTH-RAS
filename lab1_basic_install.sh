027a365c3d11201858cd6af95f74b5900354e1e4
danielduberg <danielduberg@gmail.com>
2018-08-29 20:45:54 +0200

    Create lab1_basic_install.sh

---
+++ lab1_basic_install.sh
@@ -0,0 +1,29 @@
+# setup environment
+source /opt/ros/kinetic/setup.bash
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# create catkin ws
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+
+catkin_init_workspace
+wstool init
+
+cd ~/catkin_ws
+catkin_make
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/catkin_ws/devel/setup.bash
+source ~/.bashrc
+
+# merge rosinstall files
+cd ~/catkin_ws/src
+
+wget https://raw.githubusercontent.com/KTH-RAS/ras_install/kinetic-2018/rosinstall/vm.rosinstall
+wstool merge vm.rosinstall
+wstool update
+
+cd ~/catkin_ws
+source ~/.bashrc
+catkin_make
+source ~/catkin_ws/devel/setup.bash
